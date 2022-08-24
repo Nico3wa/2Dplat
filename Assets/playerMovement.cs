26c7f8d7c55cf7abf6a704be8daac00e8ed992c8
Nicolas Demelin <nicolas.demelin@hotmail.com>
2022-08-24 14:14:37 +0200

    binding + player mouvement

--- Assets/playerMovement.cs
+++ Assets/playerMovement.cs
@@ -7,9 +7,51 @@ public class playerMovement : MonoBehaviour
 {
     [SerializeField] InputActionReference _moveInput;
     [SerializeField] InputActionReference _jumpInput;
+    [SerializeField] Transform _root;
+    [SerializeField] float _speed;
+    Vector2 _playerMovement;
 
 
 
+    private void Reset()
+    {
+        _speed = 2f;
+        Debug.Log(_speed);
+    }
+
+
+    void Start()
+    {
+        // Move
+        _moveInput.action.started += StartMove;
+        _moveInput.action.performed += UpdateMove;
+        _moveInput.action.canceled += EndMove;
+    }
+
+       void FixedUpdate()
+    {
+        //Debug.Log($"Update !{ _playerMovement}");
+
+        Vector2 direction = new Vector2(_playerMovement.x, 0);
+       _root.transform.Translate( _playerMovement * Time.fixedDeltaTime * _speed);
+    }
+
+    void StartMove(InputAction.CallbackContext obj)
+    {
+        _playerMovement = obj.ReadValue<Vector2>();
+        //Debug.Log($"Appelé ! {_playerMovement}");
+    }
+
+    void UpdateMove(InputAction.CallbackContext obj)
+    {
+        _playerMovement = obj.ReadValue<Vector2>();
+        //Debug.Log($"Joystic UPDATE ! {_playerMovement}");
+    }
+    void EndMove(InputAction.CallbackContext obj)
+    {
+        _playerMovement = new Vector2(0, 0);
+        //Debug.Log($"Joystic Annulé !");
+    }
 
 
 
